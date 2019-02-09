e7b2492ebbfbc62c10eb7168b1d0c4d1ed378773
Aaron <aaron8tang@qq.com>
2019-02-10 01:44:06 +0800

    add comment

--- run-sinwave.py
+++ run-sinwave.py
@@ -24,12 +24,12 @@ def plot_results_multiple(predicted_data, true_data, prediction_len):
 #Main Run Thread
 if __name__=='__main__':
 	global_start_time = time.time()
-	epochs  = 50
+	epochs  = 1
 	seq_len = 50
 
 	print('> Loading data... ')
 
-	X_train, y_train, X_test, y_test = lstm.load_data('sp500.csv', seq_len, True)
+	X_train, y_train, X_test, y_test = lstm.load_data('sinwave.csv', seq_len, False)
 
 	print('> Data Loaded. Compiling...')
 
@@ -43,8 +43,10 @@ if __name__=='__main__':
 	    validation_split=0.05)
 
 	predictions = lstm.predict_sequences_multiple(model, X_test, seq_len, 50)
+
 	#predicted = lstm.predict_sequence_full(model, X_test, seq_len)
-	#predicted = lstm.predict_point_by_point(model, X_test)        
+	# predictions = lstm.predict_point_by_point(model, X_test)
 
 	print('Training duration (s) : ', time.time() - global_start_time)
 	plot_results_multiple(predictions, y_test, 50)
+	# plot_results(predictions,y_test)
